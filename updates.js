79e912efe0dcab93d00fa6279a81be716f02ceff
jeshicawang <jeshicawang@gmail.com>
2016-11-22 11:05:56 -0800

    Refactored javascript. #1

--- updates.js
+++ updates.js
@@ -2,34 +2,32 @@ var user = {
   username: 'jwang',
   profilePicture: 'images/profile-picture.jpg',
   aboutMe: 'Female. 20. Coffee lover. Dancer. Future software developer;)',
-  updates: [addUpdate('9am', 'Going to coding class!'),
-            addUpdate('11:30am', 'Off to my lunch break!'),
-            addUpdate('12:15pm', '...and back to class!'),
-            addUpdate('4:30', 'Class is over for the day.'),
-            addUpdate('5pm', 'Going home for dinner, gym, relaxation, dancing, whatever I feel like doing!'),]
+  updates: [newUpdate('9am', 'Starting my weekday by going to coding class!'),
+            newUpdate('11:30am', 'Off to my lunch break!'),
+            newUpdate('12:15pm', '...and back to class!'),
+            newUpdate('4:30', 'Class just ended.'),
+            newUpdate('5pm', 'Going home for the day! Maybe I\'ll go dancing?'),]
 }
 
-function addUpdate(timestamp, post) {
+function newUpdate(timestamp, post) {
   return { timestamp: timestamp, post: post };
 }
 
+function newElement(tagName, className, text) {
+  var newElement = document.createElement(tagName);
+  newElement.className = className;
+  if (text !== null)
+    newElement.appendChild(document.createTextNode(text));
+  return newElement;
+}
+
 var i;
-var updateContainer = document.getElementById('updates');
+var containerPointer = document.getElementById('updates');
+var childPointer;
 for (i = 0; i < user.updates.length; i++) {
-  var newUpdate = document.createElement('div');
-  newUpdate.className = 'update';
-  updateContainer.appendChild(newUpdate);
-  var currentUpdate = updateContainer.lastChild;
-  var newUsername = document.createElement('h4');
-  newUsername.className = 'username';
-  newUsername.appendChild(document.createTextNode(user.username));
-  var newTimestamp = document.createElement('p');
-  newTimestamp.className = 'timestamp';
-  newTimestamp.appendChild(document.createTextNode(user.updates[i].timestamp));
-  var newPost = document.createElement('p');
-  newPost.className = 'post';
-  newPost.appendChild(document.createTextNode(user.updates[i].post));
-  currentUpdate.appendChild(newUsername);
-  currentUpdate.appendChild(newTimestamp);
-  currentUpdate.appendChild(newPost);
+  containerPointer.appendChild(newElement('div', 'update', null));
+  childPointer = containerPointer.lastChild;
+  childPointer.appendChild(newElement('h4', 'username', '@' + user.username))
+  childPointer.appendChild(newElement('p', 'timestamp', user.updates[i].timestamp))
+  childPointer.appendChild(newElement('p', 'post', user.updates[i].post))
 }
