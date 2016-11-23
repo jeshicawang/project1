75128310627b7f311c25536d0fa81b8b4416cc3c
jeshicawang <jeshicawang@gmail.com>
2016-11-23 11:36:38 -0800

    Refactoring javascript. #7

--- display.js
+++ display.js
@@ -2,15 +2,19 @@ var user = {
   username: 'jeshicawang',
   profilePicture: 'images/profile-picture.jpg',
   aboutMe: 'Female. 20. Coffee lover. Argentine Tango Dancer. Future software developer;)',
-  updates: [newUpdate(moment('5:00PM 11/22/16', 'h:mmA M/D/YY'), 'I\'m going home for the day!'),
-            newUpdate(moment('4:30PM 11/22/16', 'h:mmA M/D/YY'), 'Class just ended.'),
-            newUpdate(moment('12:15PM 11/22/16', 'h:mmA M/D/YY'), '...and lunch is over, so back to class.'),
-            newUpdate(moment('11:30AM 11/22/16', 'h:mmA M/D/YY'), 'Off to my lunch break! Maybe I\'ll go acrross the street?'),
-            newUpdate(moment('9:00AM 11/22/16', 'h:mmA M/D/YY'), 'Starting my weekday by going to coding class!')]
+  updates: [newUpdate(newMoment('5:00PM 11/22/16'), 'I\'m going home for the day!'),
+            newUpdate(newMoment('4:30PM 11/22/16'), 'Class just ended.'),
+            newUpdate(newMoment('12:15PM 11/22/16'), '...and lunch is over, so back to class.'),
+            newUpdate(newMoment('11:30AM 11/22/16'), 'Off to my lunch break! Maybe I\'ll go acrross the street?'),
+            newUpdate(newMoment('9:00AM 11/22/16'), 'Starting my weekday by going to coding class!')]
 }
 
 var containerPointer = document.getElementById('updates');
 var pointer;
+
+function newMoment(timestamp) {
+  return moment(timestamp, 'h:mmA M/D/YY')  ;
+}
 
 function printMoment(timestamp) {
   return timestamp.format('h:mmA M/D/YY');
