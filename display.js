b2c86f4e901781c638de36d3cf5bc6af0ff324a9
jeshicawang <jeshicawang@gmail.com>
2016-11-23 11:31:31 -0800

    Added functional timestamps. #7

---
+++ display.js
@@ -0,0 +1,68 @@
+var user = {
+  username: 'jeshicawang',
+  profilePicture: 'images/profile-picture.jpg',
+  aboutMe: 'Female. 20. Coffee lover. Argentine Tango Dancer. Future software developer;)',
+  updates: [newUpdate(moment('5:00PM 11/22/16', 'h:mmA M/D/YY'), 'I\'m going home for the day!'),
+            newUpdate(moment('4:30PM 11/22/16', 'h:mmA M/D/YY'), 'Class just ended.'),
+            newUpdate(moment('12:15PM 11/22/16', 'h:mmA M/D/YY'), '...and lunch is over, so back to class.'),
+            newUpdate(moment('11:30AM 11/22/16', 'h:mmA M/D/YY'), 'Off to my lunch break! Maybe I\'ll go acrross the street?'),
+            newUpdate(moment('9:00AM 11/22/16', 'h:mmA M/D/YY'), 'Starting my weekday by going to coding class!')]
+}
+
+var containerPointer = document.getElementById('updates');
+var pointer;
+
+function printMoment(timestamp) {
+  return timestamp.format('h:mmA M/D/YY');
+}
+
+function newUpdate(timestamp, post) {
+  return { timestamp: timestamp, post: post };
+}
+
+function newElement(tagName, className, text) {
+  var newElement = document.createElement(tagName);
+  newElement.className = className;
+  if (text !== null)
+    newElement.appendChild(document.createTextNode(text));
+  return newElement;
+}
+
+function displayUserInfo() {
+  pointer = document.getElementById('photo');
+  pointer.style.backgroundImage = 'url(' + user.profilePicture + ')';
+  pointer = document.getElementById('username');
+  pointer.appendChild(document.createTextNode('@' + user.username));
+  pointer = document.getElementById('about-me');
+  pointer.appendChild(document.createTextNode(user.aboutMe));
+}
+
+function displayExistingUpdates() {
+  var i;
+  for (i = 0; i < user.updates.length; i++) {
+    containerPointer.appendChild(newElement('div', 'update', null));
+    displayPost(containerPointer.lastChild, i);
+  }
+}
+
+function displayPost(pointer, index) {
+  pointer.appendChild(newElement('h4', 'username', '@' + user.username));
+  pointer.appendChild(newElement('p', 'timestamp', printMoment(user.updates[index].timestamp)));
+  pointer.appendChild(newElement('p', 'post', user.updates[index].post));
+}
+
+function addUpdate() {
+  var post = document.getElementById('post-text').value;
+  user.updates.unshift(newUpdate(moment(), post));
+  containerPointer.insertBefore(newElement('div', 'update', null), containerPointer.firstChild);
+  displayPost(containerPointer.firstChild, 0);
+}
+
+function enablePosting() {
+  pointer = document.getElementById('post-button');
+  pointer.addEventListener('click', addUpdate, false);
+}
+
+displayUserInfo();
+displayExistingUpdates();
+enablePosting();
