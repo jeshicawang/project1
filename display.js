d218be14c16594b4adca79cf0a02d44b0fd9a1c5
jeshicawang <jeshicawang@gmail.com>
2016-11-27 04:39:59 -0800

    User can view homepage. #23

--- display.js
+++ display.js
@@ -1,18 +1,21 @@
 /* global moment */
 
-var users = [ { username: 'jwang',
+var users = [ { id: 0,
+                username: 'jwang',
                 firstName: 'Jessica',
                 lastName: 'Wang',
                 profilePic: 'images/jwang.jpg',
                 aboutMe: 'Female. 20. Coffee lover. Argentine Tango Dancer. Future software developer;)',
                 following: [] },
-              { username: 'biagi',
+              { id: 1,
+                username: 'biagi',
                 firstName: 'Rodolfo',
                 lastName: 'Biagi',
                 profilePic: 'images/biagi.jpg',
                 aboutMe: 'An Argentine Tango musician who started his musical career by playing background music for silent movies, and this was where he was first discovered by a tango band leader.',
                 following: [] },
-              { username: 'varela',
+              { id: 2,
+                username: 'varela',
                 firstName: 'Hector',
                 lastName: 'Varela',
                 profilePic: 'images/varela.jpg',
@@ -25,16 +28,16 @@ var currentlyViewing = primaryUser.username;
 var postUserInput = false;
 var searchUserInput = false;
 
-var updates = [ { user: 'jwang', timestamp: newMoment('5:00PM 11/22/16'), post: 'I\'m going home for the day!' },
-                { user: 'jwang', timestamp: newMoment('4:30PM 11/22/16'), post: 'Class just ended.' },
-                { user: 'varela', timestamp: newMoment('2:00PM 11/22/16'), post: 'No me hablas tesoro mio, No me hablas ni me has mirado, Fueron tres años mi vida, Tres años muy lejos de tu corazon.' },
-                { user: 'jwang', timestamp: newMoment('12:15PM 11/22/16'), post: '...and lunch is over, so back to class.' },
-                { user: 'biagi', timestamp: newMoment('12:00PM 11/22/16'), post: 'Todo es amor, la brisa y tú, jugando en el rumor, y el ruiseñor, cantando en una flor, buscando amor, amor.' },
-                { user: 'biagi', timestamp: newMoment('11:55AM 11/22/16'), post: 'La soledad, que me envuelve el corazón, va encendiendo en mi alma, el fuego de tu amor lejano. En las brumas de tu olvido, viaja mi ilusión, gritando tu nombre en vano.' },
-                { user: 'biagi', timestamp: newMoment('11:45AM 11/22/16'), post: 'Soñemos, que los dos estamos libres. Soñemos, en la gloria de este amor. Soñemos, que ya nada nos separa, y que somos cual dos almas, que nacieron para amar.' },
-                { user: 'jwang', timestamp: newMoment('11:30AM 11/22/16'), post: 'Off to my lunch break! Maybe I\'ll go acrross the street?' },
-                { user: 'jwang', timestamp: newMoment('9:00AM 11/22/16'), post: 'Starting my weekday by going to coding class!' },
-                { user: 'varela', timestamp: newMoment('7:00AM 11/21/16'), post: 'Es la historia de un amor, como no hay otro igual. Que me hizo comprender, todo el bien todo el mal, que le dio luz a mi vida, apagandola después. ¡Ay, qué vida tan oscura, corazón, sin tu amor no viviré!' } ];
+var updates = [ { userId: 0, timestamp: newMoment('5:00PM 11/22/16'), post: 'I\'m going home for the day!' },
+                { userId: 0, timestamp: newMoment('4:30PM 11/22/16'), post: 'Class just ended.' },
+                { userId: 2, timestamp: newMoment('2:00PM 11/22/16'), post: 'No me hablas tesoro mio, No me hablas ni me has mirado, Fueron tres años mi vida, Tres años muy lejos de tu corazon.' },
+                { userId: 0, timestamp: newMoment('12:15PM 11/22/16'), post: '...and lunch is over, so back to class.' },
+                { userId: 1, timestamp: newMoment('12:00PM 11/22/16'), post: 'Todo es amor, la brisa y tú, jugando en el rumor, y el ruiseñor, cantando en una flor, buscando amor, amor.' },
+                { userId: 1, timestamp: newMoment('11:55AM 11/22/16'), post: 'La soledad, que me envuelve el corazón, va encendiendo en mi alma, el fuego de tu amor lejano. En las brumas de tu olvido, viaja mi ilusión, gritando tu nombre en vano.' },
+                { userId: 1, timestamp: newMoment('11:45AM 11/22/16'), post: 'Soñemos, que los dos estamos libres. Soñemos, en la gloria de este amor. Soñemos, que ya nada nos separa, y que somos cual dos almas, que nacieron para amar.' },
+                { userId: 0, timestamp: newMoment('11:30AM 11/22/16'), post: 'Off to my lunch break! Maybe I\'ll go acrross the street?' },
+                { userId: 0, timestamp: newMoment('9:00AM 11/22/16'), post: 'Starting my weekday by going to coding class!' },
+                { userId: 2, timestamp: newMoment('7:00AM 11/21/16'), post: 'Es la historia de un amor, como no hay otro igual. Que me hizo comprender, todo el bien todo el mal, que le dio luz a mi vida, apagandola después. ¡Ay, qué vida tan oscura, corazón, sin tu amor no viviré!' } ];
 
 function newMoment(timestamp) {
   return moment(timestamp, 'h:mmA M/D/YY');
@@ -87,24 +90,24 @@ function displayProfile(user) {
         following = true;
       }
     profileContainer.appendChild(createElement('button', { id: 'follow' }, node(following ? 'Following' : 'Follow')));
-    document.getElementById('follow').addEventListener('click', function() { follow(user.username) }, false);
+    document.getElementById('follow').addEventListener('click', function() { follow(user.id) }, false);
   }
   var updatesContainer = document.getElementById('updates');
   for (i = 0; i < updates.length; i++) {
-    if (updates[i].user === user.username)
+    if (updates[i].userId === user.id)
       updatesContainer.appendChild(createElement('div', { class: 'update' }, getUpdateElements(user, i)));
   }
   currentlyViewing = user.username;
 }
 
-function follow(username) {
+function follow(id) {
   var followButton = document.getElementById('follow').firstChild;
   if (followButton.data === 'Following') {
-    var index = primaryUser.following.indexOf(username);
+    var index = primaryUser.following.indexOf(id);
     primaryUser.following.splice(index, 1);
     followButton.data = 'Follow';
   } else {
-    primaryUser.following.unshift(username);
+    primaryUser.following.unshift(id);
     followButton.data = 'Following';
   }
 }
@@ -120,6 +123,7 @@ function empty(ids) {
 }
 
 function enableEventListeners() {
+  document.getElementById('home-button').addEventListener('click', goHome, false);
   document.getElementById('profile-button').addEventListener('click', function() { switchUser(primaryUser) }, false);
   document.getElementById('post-input').addEventListener('focus', modifyPostTextbox, false);
   document.getElementById('post-input').addEventListener('blur', modifyPostTextbox, false);
@@ -127,6 +131,22 @@ function enableEventListeners() {
   document.getElementById('search-input').addEventListener('focus', modifySearchTextbox, false);
   document.getElementById('search-input').addEventListener('blur', modifySearchTextbox, false);
   document.getElementById('search-button').addEventListener('click', checkSearchInput, false);
+}
+
+function goHome() {
+  empty(['profile', 'updates']);
+  var updatesContainer = document.getElementById('updates');
+  for (var x = 0; x < updates.length; x++) {
+    for(var y = 0; y < primaryUser.following.length; y++) {
+      if (primaryUser.following[y] === updates[x].userId)
+        updatesContainer.appendChild(createElement('div', { class: 'update' }, getUpdateElements(users[updates[x].userId], x)));
+    }
+  }
+}
+
+function switchUser(user) {
+  empty(['profile', 'updates']);
+  displayProfile(user);
 }
 
 function modifyPostTextbox() {
@@ -139,6 +159,21 @@ function modifyPostTextbox() {
     textbox.value = 'Type a new update...';
     textbox.style.color = '#b2b2b2';
     postUserInput = false;
+  }
+}
+
+function addUpdate() {
+  if (postUserInput) {
+    var updatesContainer = document.getElementById('updates');
+    var post = document.getElementById('post-input');
+    if (post.value.trim()) {
+      updates.unshift({ userId: primaryUser.id, timestamp: moment(), post: post.value });
+      if (currentlyViewing === primaryUser.username)
+        updatesContainer.insertBefore(createElement('div', { class: 'update'}, getUpdateElements(primaryUser, 0)), updatesContainer.firstChild);
+      post.value = 'Type a new update...';
+      post.style.color = '#b2b2b2';
+      postUserInput = false;
+    }
   }
 }
 
@@ -155,21 +190,6 @@ function modifySearchTextbox() {
   }
 }
 
-function addUpdate() {
-  if (postUserInput) {
-    var updatesContainer = document.getElementById('updates');
-    var post = document.getElementById('post-input');
-    if (post.value.trim()) {
-      updates.unshift({ user: primaryUser.username, timestamp: moment(), post: post.value });
-      if (currentlyViewing === primaryUser.username)
-        updatesContainer.insertBefore(createElement('div', { class: 'update'}, getUpdateElements(primaryUser, 0)), updatesContainer.firstChild);
-      post.value = 'Type a new update...';
-      post.style.color = '#b2b2b2';
-      postUserInput = false;
-    }
-  }
-}
-
 function checkSearchInput() {
   var input = document.getElementById('search-input').value;
   if (input.trim)
@@ -177,11 +197,6 @@ function checkSearchInput() {
       if (users[i].username === input)
         switchUser(users[i]);
     }
-}
-
-function switchUser(user) {
-  empty(['profile', 'updates']);
-  displayProfile(user);
 }
 
 displayProfile(primaryUser);
